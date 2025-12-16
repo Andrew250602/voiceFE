786117fb49f5f5b29a401160ac2d2785d1e45b4e
trieuvy <trieuvy.nguyen@vn.wilmar-intl.com>
2025-12-16 14:56:04 +0700

    deploy 13

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { HashRouter } from 'react-router-dom' // SỬ DỤNG HashRouter
+import React from 'react'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <HashRouter basename="/voiceFE"> {/* SỬ DỤNG HashRouter */}
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 )
